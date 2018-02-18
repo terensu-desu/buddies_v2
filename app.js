7611fc880e7ae1f47a68c007c2f778441d42d7cb
Terence Mangram <tmangram@gmail.com>
2018-02-18 16:26:37 +0900

    Debugging

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(function(req, res, next) {
 app.use("/services", services)
 app.use('/users', users);
 app.use(index);
-
+/*
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -74,7 +74,7 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
+});*/
 
 app.listen(port, function() {
   console.log("Buddies is now running and listening on port: " + port);
