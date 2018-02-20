930030c59ed2e8d9d6d282c3be02f7820e0677d2
Terence Mangram <tmangram@gmail.com>
2018-02-20 22:32:23 +0900

    Add function to check if user owns service listing

--- middleware/index.js
+++ middleware/index.js
@@ -10,4 +10,25 @@ middlewareObj.isLoggedIn = function(req, res, next) {
 	res.redirect('/login');
 }
 
+middlewareObj.checkListingOwnerShip = function(req, res, next) {
+	if(req.isAuthenticated()) {
+		Service.findById(req.params.id, function(err, foundService) {
+			if(err) {
+				req.flash("negative", "Sorry, that service listing doesn't exist!");
+				res.redirect("/");
+			} else {
+				if(foundService.author.id.equals(req.user._id)) {
+					next();
+				} else {
+					req.flash("negative", "Authorization error.");
+					res.redirect("/");
+				}
+			}
+		});
+	} else {
+		req.flash("negative", "Please login first.");
+		res.redirect("/login");
+	}
+}
+
 module.exports = middlewareObj;
