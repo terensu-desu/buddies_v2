f86041e8cb516744eee2c0af91aa97f8dfa073fd
Terence Mangram <tmangram@gmail.com>
2018-02-24 19:17:43 +0900

    Add middleware to check review ownership

--- middleware/index.js
+++ middleware/index.js
@@ -10,14 +10,35 @@ middlewareObj.isLoggedIn = function(req, res, next) {
 	res.redirect('/login');
 }
 
-middlewareObj.checkListingOwnerShip = function(req, res, next) {
+middlewareObj.checkListingOwnership = function(req, res, next) {
 	if(req.isAuthenticated()) {
 		Service.findById(req.params.id, function(err, foundService) {
-			if(err) {
+			if(err || !foundService) {
 				req.flash("negative", "Sorry, that service listing doesn't exist!");
 				res.redirect("/");
 			} else {
 				if(foundService.author.id.equals(req.user._id)) {
+					next();
+				} else {
+					req.flash("negative", "Authorization error.");
+					res.redirect("/");
+				}
+			}
+		});
+	} else {
+		req.flash("negative", "Please login first.");
+		res.redirect("/login");
+	}
+}
+
+middlewareObj.checkReviewOwnership = function(req, res, next) {
+	if(req.isAuthenticated()) {
+		Review.findById(req.params.review_id, function(err, foundReview) {
+			if(err || !foundReview) {
+				req.flash("negative", "Sorry, that review doesn't exist!");
+				res.redirect("/");
+			} else {
+				if(foundReview.author.id.equals(req.user._id)) {
 					next();
 				} else {
 					req.flash("negative", "Authorization error.");
